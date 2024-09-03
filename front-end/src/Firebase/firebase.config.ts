bd9546aee77a48dd9fe3fa6d831a28713f2a8b81
Sadananda <84262361+Akul-sada@users.noreply.github.com>
2024-09-03 04:00:31 -0700

    Delete front-end/src/Firebase/firebase.config.ts

--- front-end/src/Firebase/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBsMmYRYvwcKUrZRO6gDU_epVrdGvn1pjY",
-    authDomain: "clinic-management-system-ddce8.firebaseapp.com",
-    projectId: "clinic-management-system-ddce8",
-    storageBucket: "clinic-management-system-ddce8.appspot.com",
-    messagingSenderId: "806696678691",
-    appId: "1:806696678691:web:5a6a8e852b292f6e978896"
-  };
